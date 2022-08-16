6e0043fb3a5f441cc2b49985f02e6a8dcec93a07
Braiam Tejeda <braiam.tejeda@gmail.com>
2022-08-16 18:13:10 -0500

    Hasta 194. Hero Card

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages';
+import { Navbar } from '../../common';
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+
+            <div className="container mt-3">
+                <Routes>
+                    <Route path="marvel" element={<MarvelPage />} />
+                    <Route path="dc" element={<DcPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                    <Route path="hero" element={<HeroPage />} />
+
+                    <Route path="/" element={<Navigate to={"/marvel"} />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
