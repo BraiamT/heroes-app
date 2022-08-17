744cd44aa94de4d049aaecc5a69e15e5cc715137
Braiam Tejeda <braiam.tejeda@gmail.com>
2022-08-17 18:45:06 -0500

    Fin Seccion 15

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from '../../common/hooks/useForm';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
 
+    const { login } = useContext( AuthContext );
     const navigate = useNavigate();
 
+    const { onInputChange, txtUsername } = useForm({ txtUsername: '' });
+
     const onLogin = () => {
-        navigate('/', {
+        if ( txtUsername.trim().length <= 1 ) return;
+
+        const lastPath = localStorage.getItem( 'lastPath' ) || '/';
+
+        login( txtUsername );
+
+        navigate( lastPath, {
             replace: true
         });
     }
@@ -15,10 +27,29 @@ export const LoginPage = () => {
             <h1>Login</h1>
             <hr />
 
-            <button
-                className="btn btn-primary"
-                onClick={ onLogin }
-            >Login</button>
+            <form onSubmit={ onLogin }>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="username"
+                            name="txtUsername"
+                            value={ txtUsername }
+                            onChange={ onInputChange }
+                        />
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <button
+                        className="btn btn-primary mt-2"
+                        onClick={ onLogin }
+                        type="submit"
+                        disabled={ txtUsername.length <= 1 }
+                    >Login</button>
+                </div>
+            </form>
         </div>
     )
 }
